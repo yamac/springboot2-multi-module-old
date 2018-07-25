586d52763893c4a11621ef891fe6b58019a28ad3
yamac <dev@yamac.net>
2018-07-26 02:08:26 +0900

    initial

---
+++ web/src/test/java/net/yamac/springboot2/example/ApplicationTest.java
@@ -0,0 +1,22 @@
+package net.yamac.springboot2.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+    @Autowired
+    private ExampleService exampleService;
+
+    @Test
+    public void contextLoads() {
+        assertThat(exampleService).isNotNull();
+    }
+}
