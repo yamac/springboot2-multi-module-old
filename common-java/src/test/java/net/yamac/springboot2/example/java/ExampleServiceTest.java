61de272828b7286244c3d91124a5eb6754e9f17e
yamac <dev@yamac.net>
2018-08-31 02:23:18 +0900

    Rename current module with hyphen-java suffix

---
+++ common-java/src/test/java/net/yamac/springboot2/example/java/ExampleServiceTest.java
@@ -0,0 +1,23 @@
+package net.yamac.springboot2.example.java;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleServiceTest {
+
+    @Autowired
+    private ExampleService exampleService;
+
+    @Test
+    public void testGetMessageReturnsCorrectValue() {
+        assertThat(exampleService.getMessage()).isNotNull();
+        assertThat(exampleService.getMessage()).contains("Hello World").contains("Test");
+    }
+}
---
+++ web-java/src/main/java/net/yamac/springboot2/example/java/ExampleController.java
@@ -0,0 +1,17 @@
+package net.yamac.springboot2.example.java;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ExampleController {
+
+    private final ExampleService exampleService;
+
+    @GetMapping("/")
+    public String getIndex() {
+        return exampleService.getMessage();
+    }
+}
